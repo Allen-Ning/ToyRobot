54eb87fee48a75c5b7cc24465bfac6c7a30d9780
Allen Ning <npc1222@icloud.com>
2017-02-11 01:28:20 +1100

    added board class

---
+++ lib/entity/board.rb
@@ -0,0 +1,20 @@
+module ToyRobot
+  class Board
+    attr_accessor :width, :height
+
+    WIDTH  = 5
+    HEIGHT = 5
+
+    # @param {Integer} width
+    # @param {Integer} height
+    def initialize(width = WIDTH, height = HEIGHT)
+      if width > 0 and height > 0
+        @width  = width
+        @height = height
+      else
+        @width  = WIDTH
+        @height = HEIGHT
+      end
+    end
+  end
+end
