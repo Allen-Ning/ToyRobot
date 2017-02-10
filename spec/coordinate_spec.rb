4119d088e4c508ee3e8a2819cac7b4ad052a0db1
Allen Ning <npc1222@icloud.com>
2017-02-11 02:42:00 +1100

    added coordinate test

---
+++ spec/coordinate_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+module ToyRobot
+  describe Coordinate do
+    let(:random) { Random.new }
+
+    it 'add coordinate' do
+      x1 = random.rand(-Random.new_seed..Random.new_seed)
+      y1 = random.rand(-Random.new_seed..Random.new_seed)
+
+      x2 = random.rand(-Random.new_seed..Random.new_seed)
+      y2 = random.rand(-Random.new_seed..Random.new_seed)
+
+      coordinate = Coordinate.new(x1, y1)
+      another_coordinate = Coordinate.new(x2, y2)
+      coordinate.add_coordinate(another_coordinate)
+      expect(coordinate.x).to eql(x1 + x2)
+      expect(coordinate.y).to eql(y1 + y2)
+    end
+  end
+end
