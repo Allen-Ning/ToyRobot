86ba7f9968e999d46ca1066b10a190b1f59d35a8
Allen Ning <npc1222@icloud.com>
2017-02-11 18:17:34 +1100

    added tests for command processor

---
+++ spec/command_processor_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+module ToyRobot
+  describe CommandProcessor do
+    let(:random) { Random.new }
+    let(:robot) { Robot.new }
+    let(:board) { Board.new }
+    let(:command_processor) { CommandProcessor.new(robot, board) }
+
+    context 'should process command' do
+      it 'should process PLACE command' do
+        expect(robot).to receive(:set_position)
+        command_processor.process_command('PLACE 2,3,NORTH')
+      end
+
+      it 'should call turn left method' do
+        expect(robot).to receive(:turn_left)
+        command_processor.process_command('LEFT')
+      end
+
+      it 'should call turn right method' do
+        expect(robot).to receive(:turn_right)
+        command_processor.process_command('RIGHT')
+      end
+
+      it 'should call move method' do
+        expect(robot).to receive(:move)
+        command_processor.process_command('MOVE')
+      end
+
+      it 'should call report method' do
+        expect(robot).to receive(:report)
+        command_processor.process_command('REPORT')
+      end
+
+      it 'should ignore blank lines' do
+        expect(command_processor.process_command('')).to be_nil
+      end
+
+      it 'should raise CommandParseException for invalid commands' do
+        expect{ command_processor.process_command('INVALID_COMMAND') }.to raise_error(CommandParseException)
+      end
+
+      it 'should raise CommandParseException for PLACE commands which have not 3 parameters' do
+        expect{ command_processor.process_command('PLACE 2,3,4,NORTH') }.to raise_error(CommandParseException)
+      end
+
+      it 'should raise CommandParseException for PLACE commands with wrong type coordinates' do
+        expect{ command_processor.process_command('PLACE one,two,NORTH') }.to raise_error(CommandParseException)
+      end
+    end
+  end
+end
