70efcdb43bcc0467755a038af1516585c3820c8b
Allen Ning <npc1222@icloud.com>
2017-02-12 05:29:12 +1100

    added place method instead of set_position

--- lib/simulator/command_processor.rb
+++ lib/simulator/command_processor.rb
@@ -8,8 +8,8 @@ module ToyRobot
     # @param {Robot} robot
     # @param {Board} board
     def initialize(robot, board)
+      robot.set_board(board)
       @robot = robot
-      @board = board
     end
 
     # @param  {String}   raw_command
@@ -20,8 +20,8 @@ module ToyRobot
       return if command_match.size == 0
       case command_match[0]
         when 'PLACE'
-          coordinate, direction = get_place_command_parameters(command_match[1])
-          @robot.set_position(@board, coordinate, direction)
+          direction, x, y = get_place_command_parameters(command_match[1])
+          @robot.place(direction, x, y)
         when 'LEFT'
           @robot.turn_left
         when 'RIGHT'
@@ -41,29 +41,28 @@ module ToyRobot
     def get_place_command_parameters(params_str)
       params = params_str.split(',').map(&:strip)
       raise CommandParseException, 'place command must contain x,y,direction' unless params.size == 3
-      coordinate = get_coordinates(params)
-      direction  = get_direction(params)
-      return coordinate, direction
+      x, y = get_coordinates(params)
+      direction = get_direction_name(params)
+      return direction, x, y
     end
 
     # @param  {String[]} params
-    # @return {Coordinate}
+    # @return {Integer[]}
     # @raise  {CommandParseException}
     def get_coordinates(params)
       x = Integer(params[0])
       y = Integer(params[1])
-      coordinate = Coordinate.new(x, y)
-      return coordinate
+      return x, y
     rescue ArgumentError
       raise CommandParseException, 'place coordinates must be integers'
     end
 
     # @param {String[]}
-    # @param {Direction}
-    def get_direction(params)
-      Direction.get_direction(params[2].to_sym)
+    # @param {Symbol}
+    def get_direction_name(params)
+      params[2].to_sym
     end
 
-    private :get_place_command_parameters, :get_coordinates, :get_direction
+    private :get_place_command_parameters, :get_coordinates, :get_direction_name
   end
 end
